088535241fa25c83e0dec1c8823dd2dc85bb448d
tmcnmert <tmcnmert@gmail.com>
2026-08-20 12:51:23 +0300

    feat: add category CRUD and layout management

---
+++ StoreFlow/Controllers/CategoryController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreFlow.Context;
+using StoreFlow.Entities;
+using System.Diagnostics.CodeAnalysis;
+
+namespace StoreFlow.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly StoreContext _context;
+
+        public CategoryController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult CategoryList()
+        {
+            var values = _context.Categories.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+        [HttpGet]
+        public IActionResult UpdateCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+
+
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            if (value != null)
+            {
+                _context.Categories.Remove(value);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("CategoryList");
+        }
+    }
+}
