1305b1ae1c706810b176b2f80a53781dde48bac0
tmcnmert <tmcnmert@gmail.com>
2026-09-22 17:15:40 +0300

    feat: add dynamic dashboard ViewComponents
    Replaced static dashboard content in Index.cshtml with dynamic
    ViewComponent invocations. Added ViewComponents and partial views
    for the last 6 messages and products, fetching data from
    StoreContext. Added SkipLast method usage for retrieving the
    latest records and introduced the "İstatistikler" menu item.
    Improved dashboard maintainability by modularizing its content.

---
+++ StoreFlow/ViewComponents/_Last5MessagesDashboardComponentPartial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreFlow.Context;
+
+namespace StoreFlow.ViewComponents
+{
+    public class _Last5MessagesDashboardComponentPartial:ViewComponent
+    {
+        private readonly StoreContext _context;
+        public _Last5MessagesDashboardComponentPartial(StoreContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var values = _context.Messages.OrderBy(x => x.MessageId).ToList().TakeLast(5).ToList();
+            return View(values);
+        }
+    }
+}
---
+++ StoreFlow/ViewComponents/_Last5ProductstDashboardComponentPartial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreFlow.Context;
+
+namespace StoreFlow.ViewComponents
+{
+    public class _Last5ProductstDashboardComponentPartial:ViewComponent
+    {
+        public StoreContext _context;
+        public _Last5ProductstDashboardComponentPartial(StoreContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var values = _context.Products.OrderBy(x => x.ProductId).ToList().SkipLast(6).ToList().TakeLast(5).ToList();
+            return View(values);
+        }
+    }
+}
